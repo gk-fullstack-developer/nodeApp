ec99d902bc6d5abb19e29c02dc77f273bfc71b58
Girjesh Upadhayay <girjeshu@gmail.com>
2018-07-24 11:44:36 +0530

    Files uploaded

---
+++ app.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+var con = require('./connection.js');
+//var express = require('express');
+//var app = express();
+http.createServer(function(req,res){
+	 //res.writeHead(200, {'Content-Type': 'text/html'});
+     //res.write('<h1>Hello world</>');
+     //return res.end();
+     
+	  var sql = "INSERT INTO customers (name, address) VALUES ?";
+	  var values = [
+				    ['John', 'Highway 71'],
+				    ['Peter', 'Lowstreet 4'],
+				    ['Amy', 'Apple st 652'],
+				    ['Hannah', 'Mountain 21'],
+				    ['Michael', 'Valley 345'],
+				    ['Sandy', 'Ocean blvd 2'],
+				    ['Betty', 'Green Grass 1'],
+				    ['Richard', 'Sky st 331'],
+				    ['Susan', 'One way 98'],
+				    ['Vicky', 'Yellow Garden 2'],
+				    ['Ben', 'Park Lane 38'],
+				    ['William', 'Central st 954'],
+				    ['Chuck', 'Main Road 989'],
+				    ['Viola', 'Sideway 1633']
+				  ];
+	  con.query(sql,[values], function (err, result) {
+	    if (err) throw err;
+	    console.log("1 record inserted");
+	  });
+	
+	if(req.url === "/index"){
+	   fs.readFile("index.html", function (err, data) {
+	      res.writeHead(200, {'Content-Type': 'text/html'});
+	      res.write(data);
+	      res.end();
+	   });
+	}
+     
+     
+}).listen(5000);
+
+/*app.get('/save',function(req,res){
+    var post  = {from:'me', to:'you', msg:'hi'};
+    db.query('INSERT INTO messages SET ?', post, function(err, result) {
+      if (err) throw err;
+    });
+});*/
+
+
---
+++ fileupload.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+	    form.parse(req, function (err, fields, files) {
+	      var oldpath = files.filetoupload.path;
+	      var newpath = 'upload/' + files.filetoupload.name;
+	      fs.rename(oldpath, newpath, function (err) {
+	        if (err) throw err;
+	        res.write('File uploaded and moved!');
+	        res.end();
+	      });
+	 	});
+	 	
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload" multiple><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+     res.end();
+  }
+//})
+
+//http.createServer(function(req,res){
+	if(req.url=='file123'){
+		var form = formidable.IncomingForm();
+		form.parse(req,function(error,fields,files){
+			var old = files.userfile.path;
+			var new1 = 'upload/'+files.userfile.name;
+			fs.rename(old,new1,function(err){
+				if(err) throw err;
+				res.write('fileuploaded success');
+				res.end();
+			});
+		});
+	}else{
+		fs.readFile('index.html',function(error,data){
+			res.writeHead(200,{'Content-Type': 'text/html'});
+			res.write(data);
+			 res.end();
+		});
+	}	
+}).listen(8080);;
